797a0258b2b45e7a1fd94d697344367044508b39
ogierpaulmck <paul_ogier@mckinsey.com>
2023-11-22 10:53:02 +0100

    ..

--- git_deployment/merge_code_from_main_to_all_prod_targets.sh
+++ git_deployment/merge_code_from_main_to_all_prod_targets.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+repo_root=$(git rev-parse --show-toplevel)
+cd "$repo_root" || exit
+
 git checkout main
 git pull origin/main
-bash deploy_to_target.sh --target standard
-bash deploy_to_target.sh --target client1
-bash deploy_to_target.sh --target client2
+bash git_deployment/merge_code_from_ref_to_target.sh --reference main --target standard
+bash git_deployment/merge_code_from_ref_to_target.sh --reference main --target client1
+bash git_deployment/merge_code_from_ref_to_target.sh --reference main --target client2
 git checkout main
