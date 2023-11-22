05c56d46d9d77a15df92d4e1d59b36bb1f948399
ogierpaulmck <paul_ogier@mckinsey.com>
2023-11-22 15:24:59 +0100

    custom supplier  names

--- cb_customizations/models/2_custom/suppliers_dc9.sql
+++ cb_customizations/models/2_custom/suppliers_dc9.sql
@@ -1,5 +1,7 @@
 SELECT
-{{dbt_utils.star(ref('suppliers_raw'), relation_alias='a')}}
+{{dbt_utils.star(ref('suppliers_raw'), relation_alias='a', except=['supplier_name'])}},
+COALESCE(b.custom_supplier_name, a.supplier_name) as supplier_name
 
 FROM
 {{ref('suppliers_raw')}} AS a
+LEFT JOIN {{ref('custom_supplier_names')}} b on a.supplier_id = b.supplier_id
