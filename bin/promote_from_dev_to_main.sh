9034f1bd674617c6656a2678bcef1c6f94797a30
ogierpaulmck <paul_ogier@mckinsey.com>
2023-11-28 14:15:50 +0100

    fixed repo directory structure

--- bin/promote_from_dev_to_main.sh
+++ bin/promote_from_dev_to_main.sh
@@ -20,7 +20,7 @@ git pull origin "$dev_branch" || exit
 
 
 # Run tests or any other validation steps here
-bash dbt_run/test_dbt_seed_run_test.sh --target "$dev_branch" || exit
+bash tests/test_dbt_build_full.sh --target "$dev_branch" || exit
 
 git add .
 git commit -m "message"
@@ -30,7 +30,7 @@ git push origin "$dev_branch" || exit
 # If tests pass, move to the stage branch
 git checkout "$stage_branch" || exit
 git merge --no-ff "$dev_branch" -m "Merge $dev_branch into $stage_branch" || exit
-bash dbt_run/test_dbt_seed_run_test.sh --target "$stage_branch" || exit
+bash tests/test_dbt_build_full.sh --target "$stage_branch" || exit
 git push origin "$stage_branch" || exit
 
 # If deploying to a stage environment, add deployment steps here
