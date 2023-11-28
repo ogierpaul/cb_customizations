aff74bc7112f047a122d06491894b5b35403d202
ogierpaulmck <paul_ogier@mckinsey.com>
2023-11-28 19:12:01 +0100

    Updated artifacts

---
+++ bin/dev_merge_build_push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+repo_root=$(git rev-parse --show-toplevel)
+cd "$repo_root"/dbt || exit
+
+git merge origin/dev -m "Update from origin"
+dbt build --select state:modified+ --target dev --state=artifacts/origin || exit
+dbt clean
+dbt compile --target dev --target-path artifacts/local
+dbt compile --target dev --target-path artifacts/origin
+
+cd "$repo_root"
+git add .
+git commit -m "Updated artifacts"
+git push origin dev
+
+
--- dbt/artifacts/origin/compiled/sql_views_example/models/3_reports/spend_by_supplier.sql
+++ dbt/artifacts/origin/compiled/sql_views_example/models/3_reports/spend_by_supplier.sql
@@ -2,7 +2,7 @@
 
 SELECT
 b.supplier_name,
-SUM(a.AMOUNT) as amount
+SUM(a.AMOUNT) * 1.15 as amount
 FROM CB_DEV.DEV.invoices_dc9 as a
 LEFT JOIN CB_DEV.DEV.suppliers_dc9 as b ON a.supplier_id = b.supplier_id
 GROUP BY supplier_name
