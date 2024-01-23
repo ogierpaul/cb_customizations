a72d94a02ecb6a633ceea66d24dbc3c4141178ce
ogierpaulmck <paul_ogier@mckinsey.com>
2024-01-23 09:02:03 +0100

    Demo

--- dbt/models/1_standard/11_standard_transformations/invoices_transformed1.sql
+++ dbt/models/1_standard/11_standard_transformations/invoices_transformed1.sql
@@ -1,5 +1,6 @@
 SELECT
 dim_invoice::INT AS dim_invoice,
 supplier_id::INT AS supplier_id,
-amount::FLOAT as amount
+amount::FLOAT as amount,
+1.25 * amount ::FLOAT as amount_usd
 FROM {{ref('seeds_source_invoices')}}
